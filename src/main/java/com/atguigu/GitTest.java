0bb26c37f7b3aeee1c02fc25742ca671b479516e
zz <zz582011554@163.com>
2022-04-12 21:46:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author lsxsln
+ * @create 2022-04-12 16:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
