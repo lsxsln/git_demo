4e5fe59e296d2bfbaa86201cfec8c39ef9bdf5b5
zz <zz582011554@163.com>
2022-04-12 22:16:01 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("test");
         System.out.println("test master");
         System.out.println("test hot-fix");
+        System.out.println("testpush");
     }
 }
